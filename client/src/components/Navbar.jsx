fe7f7cd95eaef71b846b98ed5121cc5e30ae5841
mohammadbismail <m.basim.ismail@gmail.com>
2023-01-02 15:26:32 +0200

    upd

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,27 @@
-import { Avatar, Button } from '@mui/material'
 import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
-import React, { useState } from "react";
+import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, IconButton, Toolbar, Typography, Tabs, Tab, styled } from '@material-ui/core';
-import { Icon, InputBase } from "@mui/material";
+import { AppBar, Toolbar, Typography, styled, Tab, Tabs, Avatar } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom'
 // date fns
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
-const Search = styled("div")(({ theme }) => ({
-  backgroundColor: "white",
-  padding: "0 10px",
-  borderRadius: theme.shape.borderRadius,
-  width: "32%",
-}));
+// import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+// const Search = styled("div")(({ theme }) => ({
+//   backgroundColor: "white",
+//   padding: "0 10px",
+//   borderRadius: theme.shape.borderRadius,
+//   width: "32%",
+// }));
 
 const useStyles = makeStyles((theme) => ({
   appbarWrapper: {
-    width: '80%',
+    width: '90%',
     margin: '0 auto',
+
   },
   appbarTitle: {
     flexGrow: '1',
+    cursor: 'pointer',
   },
 
   colorText: {
@@ -35,17 +36,18 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     backgroundColor: '#063970',
   },
-  search: {
-    backgroundColor: "white",
-    padding: "0 10px",
-    borderRadius: theme.shape.borderRadius,
-    width: "40%",
-  },
+  // search: {
+  //   backgroundColor: "white",
+  //   padding: "0 10px",
+  //   borderRadius: theme.shape.borderRadius,
+  //   width: "40%",
+  // },
   Tab: {
     fontSize: '1.1rem',
   },
 }));
 const Navbar = () => {
+  const navigate = useNavigate();
   // destruct logout from custom hook useLogout
   const { logout } = useLogout()
   const { user } = useAuthContext()
@@ -61,22 +63,21 @@ const Navbar = () => {
       <React.Fragment>
         <AppBar className={classes.appBar}>
           <Toolbar className={classes.appbarWrapper}>
-            <h1 className={classes.appbarTitle}>
+            <h1 className={classes.appbarTitle} onClick={() => navigate('/')}>
               we<span className={classes.colorText}>Met</span>
             </h1>
-            <Search className={classes.search} >
+            {/* <Search className={classes.search} >
               <InputBase placeholder="search..." />
-            </Search>
+            </Search> */}
             <Tabs sx={{ marginLeft: "auto" }} indicatorColor="secondary" textColor="inherit">
-              <Tab className={classes.Tab} label="Main" onClick={(() => openInNewTab("http://localhost:3000"))} />
-              <Tab className={classes.Tab} label="Sign In" onClick={() => openInNewTab("http://localhost:3000/login")} />
-              <Tab className={classes.Tab} label="Home" onClick={() => openInNewTab("http://localhost:3000/register")} />
-              <Tab className={classes.Tab} label="Logout" onClick={handleLogout} />
+              <Tab className={classes.Tab} label="Groups" onClick={() => navigate("/groups")} />
+              {!user ? <Tab className={classes.Tab} label="Sign In" onClick={() => navigate("/login")} /> : <Tab className={classes.Tab} label="Logout" onClick={handleLogout} />}
+              <Tab><Avatar src="" >Hi</Avatar></Tab>
             </Tabs>
           </Toolbar>
         </AppBar>
       </React.Fragment>
-    </div>
+    </div >
 
     // <div style={{ display: 'flex' }}>
     //   <Button
